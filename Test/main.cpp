abb5cdde18593ac1d943ecd304d464460670fdfd
Takehiro M <takeknock@gmail.com>
2016-06-12 21:57:31 +0900

    use PayoffCall and PayoffPut

--- Test/main.cpp
+++ Test/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include "../JoshiDPDP/functions.h"
+#include "../JoshiDPDP/PayoffCall.h"
+#include "../JoshiDPDP/PayoffPut.h"
 //#include "../JoshiDPDP/Payoff.h"
 
 int main()
@@ -8,16 +10,14 @@ int main()
     const double strike = 100.0;
     const double spot = 100.0;
     const double volatility = 0.2;
-    const double interestRate = 0.1;
+    const double interestRate = 0.01;
     const size_t numberOfPath = 10;
-    const mc::Payoff callPayoff(strike, mc::Payoff::call);
-    const mc::Payoff putPayoff(strike, mc::Payoff::put);
-    //const double price = mc::simpleMonteCarlo(maturity, strike, spot, volatility, interestRate, numberOfPath);
+    const mc::PayoffCall callPayoff(strike);
+    const mc::PayoffPut putPayoff(strike);
     const double callPrice = mc::simpleMonteCarlo(callPayoff, maturity, spot, volatility, interestRate, numberOfPath);
     const double putPrice = mc::simpleMonteCarlo(putPayoff, maturity, spot, volatility, interestRate, numberOfPath);
     std::cout << "the price of call option: " << callPrice << std::endl;
     std::cout << "the price of put option: " << putPrice << std::endl;
-
 
 
     double a;
