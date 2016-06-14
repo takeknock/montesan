9c616f2e4dc9f36270a5e8369ac67776dbccd30d
Takehiro M <takeknock@gmail.com>
2016-06-14 21:14:18 +0900

    add functionsTest class for functions in JoshiDPDP.

--- Test/functionsTest.cpp
+++ Test/functionsTest.cpp
@@ -1,3 +1,22 @@
+
+
 #include "functionsTest.h"
 
+
+CPPUNIT_TEST_SUITE_REGISTRATION(FunctionsTest);
+
+void FunctionsTest::setUp()
+{
+
+}
+
+void FunctionsTest::testtest()
+{
+    CPPUNIT_ASSERT_EQUAL(0, 1);
+}
+
+void FunctionsTest::tearDown()
+{
+
+}
 
--- Test/functionsTest.h
+++ Test/functionsTest.h
@@ -1,9 +1,24 @@
 #pragma once
 
 
-#include "cppunit/extensions/HelperMacros.h"
+#include <cppunit/extensions/HelperMacros.h>
 
-class CalcuratorTest : public CppUnit::TestFixture {
+#include "../JoshiDPDP/functions.h"
+
+
+class FunctionsTest : public CPPUNIT_NS::TestFixture {
+    CPPUNIT_TEST_SUITE(FunctionsTest);
+    CPPUNIT_TEST(testtest);
+    //CPPUNIT_TEST(test_diff);
+    //CPPUNIT_TEST(test_mul);
+    CPPUNIT_TEST_SUITE_END();
+
 public:
-    
+    void setUp();
+    void tearDown();
+
+private:
+    void testtest();
+
+
 };
