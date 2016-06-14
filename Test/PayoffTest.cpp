67589dae84a102cf918be1362877b381a8483a6b
Takehiro M <takeknock@gmail.com>
2016-06-14 21:39:09 +0900

    basic test class

--- Test/PayoffTest.cpp
+++ Test/PayoffTest.cpp
@@ -2,3 +2,14 @@
 #include "PayoffTest.h"
 
 
+CPPUNIT_TEST_SUITE_REGISTRATION(PayoffTest);
+
+void PayoffTest::setUp()
+{
+
+}
+
+void PayoffTest::tearDown()
+{
+
+}
--- Test/PayoffTest.h
+++ Test/PayoffTest.h
@@ -1,1 +1,17 @@
 #pragma once
+#include <cppunit/extensions/HelperMacros.h>
+
+class PayoffTest : public CPPUNIT_NS::TestFixture {
+    CPPUNIT_TEST_SUITE(PayoffTest);
+    //CPPUNIT_TEST(testtest);
+    //CPPUNIT_TEST(test_diff);
+    //CPPUNIT_TEST(test_mul);
+    CPPUNIT_TEST_SUITE_END();
+
+public:
+    void setUp();
+    void tearDown();
+
+
+
+};
