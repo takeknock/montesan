12cfba38873d21e403bd40103f9769887c291200
Takehiro M <takeknock@gmail.com>
2016-06-18 14:16:32 +0900

    add tests for PayoffCall class.

--- Test/PayoffTest.cpp
+++ Test/PayoffTest.cpp
@@ -1,11 +1,22 @@
 
 #include "PayoffTest.h"
+#include "../JoshiDPDP/Payoff.h"
+#include "../JoshiDPDP/PayoffCall.h"
+
 
 
 CPPUNIT_TEST_SUITE_REGISTRATION(PayoffTest);
 
 void PayoffTest::setUp()
 {
+
+}
+
+void PayoffTest::testPayoffCall()
+{
+    mc::PayoffCall payoffCall(30.0);
+    CPPUNIT_ASSERT_DOUBLES_EQUAL(70.0, payoffCall(100.0), 10e-6);
+    CPPUNIT_ASSERT_DOUBLES_EQUAL(0.0, payoffCall(20.0), 10e-7);
 
 }
 
--- Test/PayoffTest.h
+++ Test/PayoffTest.h
@@ -3,7 +3,7 @@
 
 class PayoffTest : public CPPUNIT_NS::TestFixture {
     CPPUNIT_TEST_SUITE(PayoffTest);
-    //CPPUNIT_TEST(testtest);
+    CPPUNIT_TEST(testPayoffCall);
     //CPPUNIT_TEST(test_diff);
     //CPPUNIT_TEST(test_mul);
     CPPUNIT_TEST_SUITE_END();
@@ -12,6 +12,8 @@ public:
     void setUp();
     void tearDown();
 
+private:
+    void testPayoffCall();
 
 
 };
