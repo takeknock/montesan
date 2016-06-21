50dc5455b4801b3ad7ffeae234af7401322924d8
Takehiro M <takeknock@gmail.com>
2016-06-22 00:00:36 +0900

    modified VanillaOption class for Payoff class changes.

--- JoshiDPDP/VanillaOption.h
+++ JoshiDPDP/VanillaOption.h
@@ -6,10 +6,12 @@ namespace mc {
     public:
         DLL_API VanillaOption(const Payoff& payoff, const double maturity);
         DLL_API const double getMaturity() const;
+        DLL_API VanillaOption& operator=(const VanillaOption& original);
         DLL_API const double getPayoff(const double spot) const;
+        ~VanillaOption();
 
     private:
-        const Payoff& _payoff;
+        Payoff* _payoff;
         const double _maturity;
     };
 
